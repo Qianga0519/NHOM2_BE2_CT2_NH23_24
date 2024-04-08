4f5c68ed0854b80c5acecc27a959073347b56611
Qiang@a0519 <97659536+Qianga0519@users.noreply.github.com>
2024-04-08 17:39:54 +0700

    fix

--- config/route_page_cps.php
+++
@@ -1,6 +0,0 @@
-<?php
-return  [
-    'clients.home', 'clients.shop', 'clients.blog', 'clients.contact',
-    'clients.regular', 'clients.blog_single', 'clients.cart', 'clients.product',
-    'clients.product_cate'
-];
